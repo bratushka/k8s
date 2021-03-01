988f4dd830c0c38b9af65210b1d7f6eee051acd0
Vasyl Bratushka <bratushka.v@gmail.com>
2021-03-01 15:21:32 +0800

    Setup k8s-graph

--- k8s-api/api/settings.py
+++ k8s-api/api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nv_p9l-tm&$)7+%mrv_^)ej*fbul)m&$c266n=^i%qf=6nn+y6'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
